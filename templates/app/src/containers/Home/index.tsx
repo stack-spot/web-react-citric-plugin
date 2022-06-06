03d502dd0a29c98ca002bf7271f2a3fe55116092
StackSpot Admin <admin@stackspot.com>
2022-06-06 17:52:53 +0000

    Update public repository

--- templates/app/src/containers/Home/index.tsx
+++ templates/app/src/containers/Home/index.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 
-import { Card, Text, Image, CardHeader, CardContent } from '@citric/core'
+import { Card, Text, CardHeader, CardContent } from '@citric/core'
 
 export const Home = () => {
   return (
     <>
       <Card>
         <CardHeader>
-          <Text children={'Thank you!'} appearance='overheader2' color="color.primary.main" />
-          <Text children={'Citric Design System'} appearance='h4' color="color.base.b10" />
+          <Text appearance='overheader2' color="color.primary.main">Thank you!</Text>
+          <Text appearance='h4' color="color.base.b10">Citric Design System</Text>
         </CardHeader>
-        <Image
-          src='https://citric.design/static/media/placeholder.aec73372.png'
-          alt='image of Citric'
-          width='100%'
-          height='170px' />
         <CardContent>
           <Text
-            children={'Thank you for using Citric Design System. Feel free to reach us as anytime !'}
             appearance='body2'
             color="color.base.b9"
-            weight="regular" />
+            weight="regular">Thank you for using Citric Design System. Feel free to reach us as anytime!</Text>
         </CardContent>
       </Card>
     </>
